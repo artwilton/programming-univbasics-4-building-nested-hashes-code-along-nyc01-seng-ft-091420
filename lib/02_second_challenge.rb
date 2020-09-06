48a787c373731fc2831949589dbf48e4d1c036e6
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 01:41:05 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,6 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
       #your key/value pairs here!
       }, 
     :capulet => {
