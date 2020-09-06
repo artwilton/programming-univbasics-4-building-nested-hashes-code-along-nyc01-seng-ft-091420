df7072ce0eace0ed20800f84a6e87881cd563b75
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 01:47:59 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -12,6 +12,9 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
+        :name => ,
+        :age => ,
+        :status => 
         #your key/value pairs here
         },
       :hero_friends => []
