ff7ac158511a6bac807f78db8a19af58bc28f67a
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 01:50:06 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -21,6 +21,8 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :matriarch => {
